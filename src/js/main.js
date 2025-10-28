eb583a1d314f9b006275c6647ae3f18770849bdf
Tetiana Furmanets <tetyana.best.89@ukr.net>
2025-10-28 14:53:16 +0100

    add styles.css

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 // src/js/main.js
 import { getImagesByQuery } from './pixabay-api.js';
-import { createGallery, clearGallery, showLoader, hideLoader } from './render-functions';
+import { createGallery, clearGallery, showLoader, hideLoader } from './render-functions.js';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -30,10 +30,10 @@ export function createGallery(images) {
           <img class="gallery-image" src="${webformatURL}" alt="${tags}" loading="lazy" />
         </a>
         <div class="info">
-          <p class="info-item"><b>Likes</b>${likes}</p>
-          <p class="info-item"><b>Views</b>${views}</p>
-          <p class="info-item"><b>Comments</b>${comments}</p>
-          <p class="info-item"><b>Downloads</b>${downloads}</p>
+          <p class="info-item"><b>Likes:</b>${likes}</p>
+          <p class="info-item"><b>Views:</b>${views}</p>
+          <p class="info-item"><b>Comments:</b>${comments}</p>
+          <p class="info-item"><b>Downloads:</b>${downloads}</p>
         </div>
       </li>
       `;
